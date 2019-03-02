b9b97477ffb3165bab33572760cc0d5259315f20
Reginaldo <reginaldolribeiro@gmail.com>
2019-03-02 15:53:47 -0300

    SASS with ReactJS

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,12 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
       }
     ]
   }
